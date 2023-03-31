aa152b792d866d554fa7d13db6ff5b45f23cc816
David Boddie <david@boddie.org.uk>
2023-03-31 19:10:59 +0200

    Applied my own colour preferences.

--- src/cmd/acme/config.h
+++ src/cmd/acme/config.h
@@ -1,17 +1,17 @@
-#define C_TAGBG			0x2B303BFF
-#define C_TAGFG			0xC0C5CEFF
-#define C_TAGHLBG		0x006CA5FF
-#define C_TAGHLFG		0xEFF1F5FF
+#define C_TAGBG			0xe0e0ffFF
+#define C_TAGFG			0x000000FF
+#define C_TAGHLBG		0x88bbddFF
+#define C_TAGHLFG		0x000000FF
 
-#define C_TXTBG			0xFFFFFFFF
-#define C_TXTFG			0x000000FF
-#define C_TXTHLBG		0x999999FF
+#define C_TXTBG			0x202020FF
+#define C_TXTFG			0xe0e0e0FF
+#define C_TXTHLBG		0xcccc99FF
 #define C_TXTHLFG		0x000000FF
 
 #define C_WINBUTTON		0x4d4d4dFF
 #define C_COLBUTTON		0x55aaaaFF
 #define C_TMPBUTTON		0x55aaaaFF
-#define C_SCROLLBG		0x999999FF
+#define C_SCROLLBG		0xaacc77FF
 
 #define C_BUTTON2HL		0x55aaaaFF
 #define C_BUTTON3HL		0x55aaaaFF
